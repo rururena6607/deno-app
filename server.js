3e4579bc681b8383e6b35a4225565d7fb4c40865
HIROSHI <139212863+rururena6607@users.noreply.github.com>
2026-08-19 09:34:38 +0900

    Initial commit

---
+++ server.js
@@ -0,0 +1,17 @@
+import { serveDir } from "jsr:@std/http@^1.1.3/file-server";
+
+Deno.serve((req) => {
+  const pathname = new URL(req.url).pathname;
+  console.log(pathname);
+
+  if (req.method === "GET" && pathname === "/welcome-message") {
+    return new Response("jigインターンへようこそ！");
+  }
+
+  return serveDir(req, {
+    fsRoot: "public",
+    urlRoot: "",
+    showDirListing: true,
+    enableCors: true,
+  });
+});
